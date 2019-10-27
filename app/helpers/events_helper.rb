905ad662dc35b72fb392684b76d6de6553471582
codeplus <codeplus@CDSS-4494.wired.duke.local>
2019-10-27 14:48:00 -0400

    changed landing page, added database events to indexing page

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
+require 'net/http'
+require 'json'
+require 'uri'
 module EventsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
 
-  root 'home#welcome'
+  root 'events#index'
 
   post "/graphql", to: "graphql#execute"
   get 'events' => 'events#index'
