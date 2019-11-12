c4445055e80adb8ebba30effea3cf24fc44b41d3
codeplus <codeplus@CDSS-4494.local>
2019-11-11 19:24:02 -0500

    page changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,8 +14,16 @@ class EventsController < ApplicationController
 	      sanitize_params: true,
     ) || return
 		#@events = @filterrific.find.page(params[:page])
-		@test = get_my_events('ml138')
-		@events = @test
+		#@test = get_json_events('ml138')
+		#@events = @test
+		@database_events = Event.all
+		@events = Array.new()
+		@database_events.each do |event|
+			#if event.hosts.pluck(:hostid).include?(current_user.netid)
+			if event.hosts.pluck(:hostid).include?("ahw26")
+				@events << event
+			end
+		end
 
 		respond_to do |format|
 	      format.html
@@ -60,6 +68,10 @@ class EventsController < ApplicationController
 
 	def create
 		val = eval( "#{ params[:event_host_subscription] }")
+
+		puts "creating for"
+		puts Host.find_by_hostid(val["subscribable_id"])
+
 		@event = Event.find_by_eventid($events_hash[val["title"]])
 		@host = Host.find_by_hostid(val["subscribable_id"])
 
@@ -94,66 +106,5 @@ class EventsController < ApplicationController
   		redirect_to events_path
 	end
 
-	private
-	  def get_my_events(netID)
-		#search both JSON feed and our own database (for future events)
-		#THIS IS WHERE WE CAN MAYBE INTEGRATE SHIB? BEFORE THIS? 
-		@myNetID = netID #currently hard coded, in the future this will be shib integrated
-		#index is what is called when we list the events from @events...
-		#instead of showing all the events in our database, we will now
-		#show all the events in Duke Calendar associated with the NetID
-		#per the global variable $myNetID
-		url = "https://calendar.duke.edu/events/index.json?&future_days=90&feed_type=simple&local=true"
-		uri = URI.parse(url)
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl = true
-		request = Net::HTTP::Get.new(uri.request_uri)
-		response = http.request(request).body
-		doc = JSON.parse(response)
-		@events_array = doc["events"]
-		@my_events = Array.new
-		#$events_hash = Hash.new
-		puts @myNetID
-		@events_array.each do |event| 
-			if event["submitted_by"] != nil
-				if event["submitted_by"][0].strip() == @myNetID
-					@my_events << event #this is a different type than line 152...
-					# @event = Event.find_by_eventid(event["title"])
-					# #@host = Host.find_by_hostid(val["subscribable_id"])
-
-					# if @event.blank? #doesn't exist
-					# 	@event = Event.new(:eventid => event["id"], :title => event["summary"], :status => "inactive", :checkintype => "unspecified")
-					# else
-					# 	#throw error
-					# end
-
-					# if @host.blank?
-					# 	@host = Host.new(:hostid => $myNetID)
-					# else
-					# 	#throw error
-					# end
-
-					# if !@event.hosts.pluck(:hostid).include?(@host.hostid)
-					# 	@event.save 
-					# 	@host.save
-					# 	@subscription = @event.subscriptions.new(subscribable: @host)
-					# 	@subscription.save
-					# end
-
-					#$my_events << @event
-				end
-			end
-		end
-
-		@database_events = Event.all
-		@database_events.each do |event|
-			if event.hosts.pluck(:hostid).include?(@myNetID)
-				puts event
-				@my_events << { "id" => event.eventid, "summary" => event.title } #this is a different type than line 120...
-			end
-		end
-
-		@my_events
-	end
 
 end
